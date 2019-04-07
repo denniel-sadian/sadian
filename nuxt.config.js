a23e77c549029ea51930e0749611db0a0925d1a7
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-04-07 10:46:33 +0800

    Ok

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,12 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description },
-      { hid: 'twitter-card', name: 'twitter:card', content: 'summary' }
+      { hid: 'twitter-card', name: 'twitter:card', content: 'summary' },
+      {
+        hid: 'twitter-preview_img',
+        name: 'twitter:image',
+        content: 'https://sadian.herokuapp.com/icon.jpg'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
