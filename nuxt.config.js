8c5c864bb37410ba1b4eea2d1c1b46d56fc5017d
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-04-27 13:49:47 +0800

    Ok

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' },
+      { rel: 'stylesheet', href: '/css/all.css' },
       { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/animate.css@3.5.1' }
     ]
   },
