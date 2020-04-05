72c5641f07b213599d7cc61bc9efdae2d58e9126
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-05 17:09:26 +0800

    Change the name

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,11 @@
     <div id="header-nav">
       <nav class="w3-bar w3-large w3-padding w3-text-white">
         <div class="w3-content">
-          <nuxt-link to="/" class="w3-bar-item light-gray-opacity w3-round-xxlarge w3-button round-on-small">
-            <img id="logo" src="~/assets/images/icon.jpg" width="25">
+          <nuxt-link
+            to="/"
+            class="w3-bar-item light-gray-opacity w3-round-xxlarge w3-button round-on-small"
+          >
+            <img id="logo" src="~/assets/images/icon.jpg" width="25" />
             <span class="w3-hide-small">Denniel Luis Sadian</span>
           </nuxt-link>
           <nuxt-link
@@ -14,7 +17,10 @@
             <i class="fa fa-coffee"></i>
             <span class="w3-hide-small">Portfolio</span>
           </nuxt-link>
-          <nuxt-link to="/blog" class="light-gray-opacity w3-bar-item w3-round-xxlarge w3-button round-on-small">
+          <nuxt-link
+            to="/blog"
+            class="light-gray-opacity w3-bar-item w3-round-xxlarge w3-button round-on-small"
+          >
             <i class="fa fa-book"></i>
             <span class="w3-hide-small">Blog</span>
           </nuxt-link>
@@ -26,7 +32,7 @@
             <span class="w3-hide-small w3-hide-medium">Contact</span>
           </button>
           <nuxt-link
-            :to="{name: 'portfolio-about'}"
+            :to="{ name: 'portfolio-about' }"
             class="light-gray-opacity w3-bar-item w3-round-xxlarge w3-button w3-right round-on-medium"
           >
             <i class="fa fa-info-circle"></i>
@@ -35,9 +41,13 @@
         </div>
       </nav>
 
-      <header class="w3-container w3-text-white w3-center w3-border-purple w3-bottombar">
+      <header
+        class="w3-container w3-text-white w3-center w3-border-purple w3-bottombar"
+      >
         <h1>Welcome to my Website!</h1>
-        <p class="w3-large">Hi, I'm Denniel! And I'm so glad that you found me!</p>
+        <p class="w3-large">
+          Hi, I'm Denniel! And I'm so glad that you found me!
+        </p>
       </header>
     </div>
 
@@ -49,52 +59,57 @@
             <span class="w3-text-gray">I love to code.</span>
           </h1>
           <p class="intro-p">
-            I mainly code in Python and JavaScript.
-            I mostly work with Web Development. I use Django for back-end and
-            Vue for front-end. I also have a little knowledge of some other languages.
-            I can also do automation like web scraping. You can find my
-            works in my
-            <nuxt-link to="/portfolio" class="w3-hover-text-purple">portfolio</nuxt-link>.
+            I mainly code in Python and JavaScript. I mostly work with Web
+            Development. I use Django for back-end and Vue for front-end. I also
+            have a little knowledge of some other languages. I can also do
+            automation like web scraping. You can find my works in my
+            <nuxt-link to="/portfolio" class="w3-hover-text-purple"
+              >portfolio</nuxt-link
+            >.
           </p>
         </div>
         <div class="w3-col l5 m5 w3-center w3-padding">
-          <img src="~/assets/images/binary.jpg" class="intro-img w3-round-xlarge w3-image">
+          <img
+            src="~/assets/images/binary.jpg"
+            class="intro-img w3-round-xlarge w3-image"
+          />
         </div>
       </div>
     </div>
 
     <div class="w3-container">
       <div class="w3-content">
-        <hr class="intro-sec-sep w3-border-purple">
+        <hr class="intro-sec-sep w3-border-purple" />
       </div>
     </div>
 
     <div class="intro-section w3-container w3-padding">
       <div class="w3-content w3-row">
         <div class="w3-col w3-center l5 m5 w3-padding w3-hide-small">
-          <img src="~/assets/images/pro.gif" class="w3-round-xlarge w3-image">
+          <img src="~/assets/images/pro.gif" class="w3-round-xlarge w3-image" />
         </div>
         <div class="w3-col l7 m7 w3-padding">
           <h1 class="intro-heading">
             I'm currently in Senior High.
-            <span
-              class="w3-text-gray"
-            >Currently waiting for graduation.</span>
+            <span class="w3-text-gray">Currently waiting for graduation.</span>
           </h1>
           <p class="intro-p">
-            After senior high, I'll take Computer Science in college. I'll continue to learn
-            new things and build incredibly using the knowledge I'll be gaining there.
+            After senior high, I'll take Computer Science in college. I'll
+            continue to learn new things and build incredibly using the
+            knowledge I'll be gaining there.
           </p>
         </div>
-        <div class="w3-col w3-center l5 m5 w3-padding w3-hide-medium w3-hide-large">
-          <img src="~/assets/images/pro.gif" class="w3-round-xlarge w3-image">
+        <div
+          class="w3-col w3-center l5 m5 w3-padding w3-hide-medium w3-hide-large"
+        >
+          <img src="~/assets/images/pro.gif" class="w3-round-xlarge w3-image" />
         </div>
       </div>
     </div>
 
     <div class="w3-container">
       <div class="w3-content">
-        <hr class="intro-sec-sep w3-border-purple">
+        <hr class="intro-sec-sep w3-border-purple" />
       </div>
     </div>
 
@@ -106,18 +121,25 @@
             <span class="w3-text-gray">I really want to.</span>
           </h1>
           <p class="intro-p">
-            I want to get a job where I can apply my skills that I love.
-            I interact nicely with other humans. As much as possible, I try to understand them
-            and learn from them as well.
+            I want to get a job where I can apply my skills that I love. I
+            interact nicely with other humans. As much as possible, I try to
+            understand them and learn from them as well.
           </p>
         </div>
         <div class="w3-col l5 m5 w3-center w3-padding">
-          <img src="~/assets/images/love_coding.png" class="intro-img w3-round-xlarge w3-image">
+          <img
+            src="~/assets/images/love_coding.png"
+            class="intro-img w3-round-xlarge w3-image"
+          />
         </div>
       </div>
     </div>
-    <Contact :show="contactShow" key="contact" @hide-contact-modal="contactShow = !contactShow"/>
-    <Footer/>
+    <Contact
+      :show="contactShow"
+      key="contact"
+      @hide-contact-modal="contactShow = !contactShow"
+    />
+    <Footer />
   </div>
 </template>
 
@@ -135,7 +157,7 @@ export default {
   },
   head() {
     return {
-      title: 'Denniel Luis Saway Sadian',
+      title: 'Dein Sadian',
       meta: [
         {
           hid: 'description',
@@ -211,7 +233,7 @@ header h1 {
   }
 }
 
-@media (max-width:600px) {
+@media (max-width: 600px) {
   .round-on-small {
     display: flex;
     justify-content: center;
@@ -224,7 +246,7 @@ header h1 {
   }
 }
 
-@media (max-width:992px) and (min-width:601px) {
+@media (max-width: 992px) and (min-width: 601px) {
   .round-on-medium {
     display: flex;
     justify-content: center;
