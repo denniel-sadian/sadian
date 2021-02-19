27e0b444db22a1c592ea68e5b120d3a45e236e5b
Denniel Luis <sadiandenniel@gmail.com>
2021-02-19 13:08:18 +0800

    Format

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,22 @@
 <template>
   <transition name="layout" mode="out-in">
-    <nuxt/>
+    <nuxt />
   </transition>
 </template>
 
 <style>
 html,
-        body {
-            height: 100%!important;
-            width: 100%!important;
-            padding: 0px;
-            margin: 0px;
-            overflow-x: hidden;
-        }
+body {
+  height: 100% !important;
+  width: 100% !important;
+  padding: 0px;
+  margin: 0px;
+  overflow-x: hidden;
+}
 
-        #__nuxt, #__layout {
-          height: 100%!important;
-          width: 100%!important;
-        }
+#__nuxt,
+#__layout {
+  height: 100% !important;
+  width: 100% !important;
+}
 </style>
